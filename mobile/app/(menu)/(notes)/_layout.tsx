ebc9a127fc7de87c41bbe92d90f5c458787dd5f2
Ashishrupani <143018180+Ashishrupani@users.noreply.github.com>
2025-10-31 09:55:14 -0500

    template for notes

--- mobile/app/(menu)/(notes)/_layout.tsx
+++ mobile/app/(menu)/(notes)/_layout.tsx
@@ -1,9 +1,23 @@
-import { Stack } from 'expo-router'
+import { Stack, Tabs } from 'expo-router'
 import React from 'react'
+import { HapticTab } from '@/components/haptic-tab';
+import { Colors } from '@/constants/theme';
 
 export default function NotesLayout() {
 
   return (
-    <Stack/>
+    <Tabs
+          screenOptions={{
+            tabBarActiveTintColor: Colors.light.tint,
+            headerShown: false,
+            tabBarButton: HapticTab,
+          }}>
+
+          </Tabs>
   )
 }
+
+
+/* Notes
+ * This layout component sets up the notes screen with a stack navigator.
+ */
---
+++ mobile/app/(menu)/(notes)/notesDashboard.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { ScrollView } from 'react-native'
+
+
+
+export default function NotesDashboard() {
+
+  return (
+    <ScrollView>
+      {/* Notes dashboard content goes here */}
+    </ScrollView>
+  )
+}
+
